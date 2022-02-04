5c2725270d95ba4127a741d04634178ab45bca9c
Anuprita Rajput <74887568+Anuprita-pro@users.noreply.github.com>
2022-02-04 10:31:40 +0530

    Add files via upload

---
+++ DuckTestDrive.java
@@ -0,0 +1,12 @@
+interface Duck
+{
+  public void quack();
+   public void fly();
+}
+class MallarDuck implements Duck
+{
+  public void quack()
+  {
+        system.out.intln("Qusck")
+   
+   
---
+++ EnumerationIteratorTestDrive.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+ class EnumerationIterator implements Iterator
+{
+  Enumeration enumeration;
+
+     public EnumerationIterator(Enumeration enumeration)
+  {
+    this.enumeration = enumeration;
+  }
+     public boolean hasNext()
+  {
+     return enumeration.hasMoreElements();
+  }
+     public Object next()
+  {
+     return enumeration.nextElement();
+  }
+      public void remove() 
+  {
+     throw new UnsupportedOperationException();
+   }
+}
+
+   class  EnumerationIteratorTestDrive
+{
+    public static void main(String args[])
+ { 
+    Vector v = new Vector(Arrays.asList(args));
+    Iterator iterator = new  EnumerationIterator(v.elements());
+       while (iterator.hasNext())
+    {
+       System.out.println(iterator.next());
+    }
+  }
+}
